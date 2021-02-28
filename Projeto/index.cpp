90a67d98619fca65659f1a988e9d77ad89904e03
Othon-Oliveira <othonoliveira08@gmail.com>
2021-02-28 10:03:43 -0300

    Forca

--- Projeto/index.cpp
+++ Projeto/index.cpp
@@ -3,7 +3,27 @@ using namespace std;
 
 int main(){
 
-    
+    int i, x, chances=6;
+    char word[15], latter, res[15];
+    cout << "\nHangman game\n";
+    cout << "Type the word whit de end dot: \n";
+    cin >> word;
 
+    for(i=0; word[i]!='.'; i++){
+            x=i;
+    }
+    while(chances!=0){
+        cout << "\nType a latter: \n";
+        cin >> latter;
+        for(i=0; i!=x; i++){
+            if(latter=word[i]){
+                res[i]=word[i];
+            }else{
+                chances--;
+                cout << "\nYou lose\n";
+            }
+        }
+    }
+    
     return 0;
 }
