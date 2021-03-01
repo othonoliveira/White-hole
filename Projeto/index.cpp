cde42891f14beca63ca72dd76ff9f079a880e470
Othon-Oliveira <othonoliveira08@gmail.com>
2021-03-01 17:36:09 -0300

    Começando

--- Projeto/index.cpp
+++ Projeto/index.cpp
@@ -3,27 +3,31 @@ using namespace std;
 
 int main(){
 
-    int i, x, chances=6;
+    int i, x, chances=6,a;
     char word[15], latter, res[15];
     cout << "\nHangman game\n";
-    cout << "Type the word whit de end dot: \n";
+    cout << "Type the word with de end dot: \n";
     cin >> word;
 
     for(i=0; word[i]!='.'; i++){
             x=i;
     }
-    while(chances!=0){
+    for(a=0; a<21; a++){
+        res[a]='_';
+    }
+    while(chances>0){
         cout << "\nType a latter: \n";
         cin >> latter;
-        for(i=0; i!=x; i++){
-            if(latter=word[i]){
+        for(i=0; i<x; i++){
+            if(latter==word[i]){
                 res[i]=word[i];
-            }else{
-                chances--;
-                cout << "\nYou lose\n";
             }
+            cout << res;
         }
+        chances--;
     }
-    
+
+    cout << "\nYou lose\n";
+  
     return 0;
 }
