4a9c32304b9d2189821a97942f810e9b045cb168
Othon-Oliveira <othonoliveira08@gmail.com>
2021-03-03 12:35:27 -0300

    Complet

--- Projeto/index.cpp
+++ Projeto/index.cpp
@@ -4,37 +4,56 @@ using namespace std;
 
 int main(){
 
-    int i, x, chances=6,a;
-    char word[15], latter, res[15];
-    cout << "\nHangman game\n";
-    cout << "Type the word with de end dot: \n";
+    char word[30], letter, secret[30];
+    int tam, i, chances, rights;
+    bool right;
+    
+    chances=6;
+    tam=0;
+    i=0;
+    right=false;
+    rights=0;
+
+    cout << "\nType a word: ";
     cin >> word;
     system("cls");
-    
-    for(i=0; word[i]!='.'; i++){
-            x=i+1;
-    }
-    for(a=0; a<x; a++){
-        res[a]='_';
-    }
-    res[x]='.';
-    cout << res[x+1];
-    while(chances>0){
-        cout << "\nType a latter: \n";
-        cin >> latter;
-        for(i=0; i<x & chances>0; i++){
-            if(latter==word[i]){
-                res[i]=word[i];
-            }
-            if(res==word){
-                cout << "\nYou won\n";
-                return 0;
-            }
-        }
-        cout << res << "\n" << word;
+
+    while(word[i]!='\0'){
+        i++;
+        tam++;
     }
 
-    cout << "\nYou lose\n";
-  
+    for(i=0; i<30; i++){
+        secret[i]='_';
+    }
+
+    while((chances > 0) && (rights < tam)){
+        cout << "Remaining attempts: " << chances << "\n\n";
+        cout << "Secret word: ";
+        for(i=0; i<tam; i++){
+            cout << secret[i];
+        }
+        cout << "\n\nType a letter: ";
+        cin >> letter;
+        for(i=0; i<tam; i++){
+            if(word[i]==letter){
+                right=true;
+                secret[i]=word[i];
+                rights++;
+            }
+        }
+        if(!right){
+            chances--;
+        }
+        right=false;
+        system("cls");
+    }
+    if(rights==tam){
+        cout << "\nYou win!\n";
+    }else{
+        cout << "\nWhat a shame, you lose!\n";
+    }
+    system("pause");
+
     return 0;
 }
